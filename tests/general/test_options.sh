8174f801214021dc2156aabcd469d3d280a5ff97
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-03-18 11:31:08 +0100

    Test help and version options

---
+++ tests/general/test_options.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -
+
+## Print a header
+SCRIPT_NAME="Test options"
+LINE=$(printf "%076s\n" | tr " " "-")
+printf "# %s %s\n" "${LINE:${#SCRIPT_NAME}}" "${SCRIPT_NAME}"
+
+## Declare a color code for test results
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NO_COLOR="\033[0m"
+
+failure () {
+    printf "${RED}FAIL${NO_COLOR}: ${1}\n"
+    exit -1
+}
+
+success () {
+    printf "${GREEN}PASS${NO_COLOR}: ${1}\n"
+}
+
+## Is vsearch installed?
+VSEARCH=$(which vsearch)
+DESCRIPTION="check if vsearch is in the PATH"
+[[ "${VSEARCH}" ]] && success "${DESCRIPTION}" || failure "${DESCRIPTION}"
+
+
+#*****************************************************************************#
+#                                                                             #
+#                        Options --version and --help                         #
+#                                                                             #
+#*****************************************************************************#
+
+## Return status should be 0 after -h (GNU standards)
+DESCRIPTION="return status should be 0 after -h"
+"${VSEARCH}" -h 2> /dev/null > /dev/null && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+## Return status should be 0 after -v (GNU standards)
+DESCRIPTION="return status should be 0 after -v"
+"${VSEARCH}" -v 2> /dev/null > /dev/null && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+exit 0
