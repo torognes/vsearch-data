b7445e3a5408008120958393b09c044ab36acfb5
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-06-01 11:55:28 +0200

    Test issue 181

--- tests/usearch_global/fixed_bugs.sh
+++ tests/usearch_global/fixed_bugs.sh
@@ -12,7 +12,7 @@ NO_COLOR="\033[0m"
 
 failure () {
     printf "${RED}FAIL${NO_COLOR}: ${1}\n"
-    exit -1
+    # exit -1
 }
 
 success () {
@@ -46,8 +46,47 @@ printf ">ref1\n%s\n" ${SEQ} > "${DATABASE}"
     success "${DESCRIPTION}" || \
         failure "${DESCRIPTION}"
 
+# Clean
+rm "${QUERY}" "${DATABASE}" "${ALNOUT}"
+
+#*****************************************************************************#
+#                                                                             #
+#     N-tails are converted to A-tails in consensus sequences (issue 181)     #
+#                                                                             #
+#*****************************************************************************#
+
+DESCRIPTION="N-tails are preserved in consensus sequences (issue 181)"
+USEARCH8="$(which usearch8)"
+INPUT=$(mktemp)
+PADDED=$(mktemp)
+CENTROIDS=$(mktemp)
+CONSENSUS=$(mktemp)
+cat > ${INPUT} <<'EOT'
+>seq1;size=1;
+CGCCCGTCGCTACTACCGATTGAATGGCTTAGTGAGACCT
+EOT
+
+"${USEARCH8}" \
+    -fastx_truncate ${INPUT}  \
+    -trunclen 50 \
+    -padlen 50 \
+    -fastaout ${PADDED} > /dev/null 2> /dev/null
+
+"${VSEARCH}" \
+  --cluster_fast ${PADDED} \
+  --id 0.97 \
+  --centroids ${CENTROIDS} \
+  --consout ${CONSENSUS} \
+  --sizein \
+  --sizeout \
+  --fasta_width 0 > /dev/null 2> /dev/null
+
+tail -n 1 ${CONSENSUS} | grep -E "N{10}$" && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
 
 ## Clean
-rm "${QUERY}" "${DATABASE}" "${ALNOUT}"
+rm "${INPUT}" "${PADDED}" "${CENTROIDS}" "${CONSENSUS}"
+
 
 exit 0
