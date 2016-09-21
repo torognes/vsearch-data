bc9bc40c8057769258121d3e371788e3a284ce44
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-09-21 14:50:45 +0200

    CIGAR alignment is "=" when the sequences are identical

---
+++ tests/unclassified.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -
+
+## Print a header
+SCRIPT_NAME="Unclassified tests"
+LINE=$(printf "%076s\n" | tr " " "-")
+printf "# %s %s\n" "${LINE:${#SCRIPT_NAME}}" "${SCRIPT_NAME}"
+
+## Declare a color code for test results
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NO_COLOR="\033[0m"
+
+failure () {
+    printf "${RED}FAIL${NO_COLOR}: ${1}\n"
+    exit -1
+}
+
+success () {
+    printf "${GREEN}PASS${NO_COLOR}: ${1}\n"
+}
+
+
+## Is vsearch installed?
+VSEARCH=$(which vsearch)
+DESCRIPTION="check if vsearch is in the PATH"
+[[ "${VSEARCH}" ]] && success "${DESCRIPTION}" || failure "${DESCRIPTION}"
+
+#*****************************************************************************#
+#                                                                             #
+#                    Clustering UC format CIGAR alignment                     #
+#                                                                             #
+#*****************************************************************************#
+
+DESCRIPTION="CIGAR alignment is \"=\" when the sequences are identical"
+FASTA=$(mktemp)
+UC_OUT=$(mktemp)
+SEQ="ACGT"
+printf ">seq1\nACGT\n>seq2\nACGT\n" > "${FASTA}"
+
+"${VSEARCH}" \
+    --cluster_fast "${FASTA}" \
+    --id 0.97 \
+    --uc "${UC_OUT}" \
+    --minseqlength 1 \
+    --quiet
+
+[[ $(grep "^H" "${UC_OUT}" | cut -f 8) == "=" ]] && \
+    success  "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+## Clean
+rm "${FASTA}" "${UC_OUT}"
+
+exit 0
