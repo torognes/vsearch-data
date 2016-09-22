a79b60eb26060311295a234b68d46108604b4338
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-09-22 09:53:05 +0200

    dereplication prefix tests (one fail)

--- tests/unclassified.sh
+++ tests/unclassified.sh
@@ -31,7 +31,7 @@ DESCRIPTION="check if vsearch is in the PATH"
 #                                                                             #
 #*****************************************************************************#
 
-# usearch 6, 7 and 8 output a "=" when the sequences are identical
+## usearch 6, 7 and 8 output a "=" when the sequences are identical
 DESCRIPTION="CIGAR alignment is \"=\" when the sequences are identical"
 UC_OUT=$("${VSEARCH}" \
              --cluster_fast <(printf ">seq1\nACGT\n>seq2\nACGT\n") \
@@ -44,7 +44,64 @@ UC_OUT=$("${VSEARCH}" \
     success  "${DESCRIPTION}" || \
         failure "${DESCRIPTION}"
 
-# clean
+## clean
 unset UC_OUT
+
+
+#*****************************************************************************#
+#                                                                             #
+#                        UC format when dereplicating                         #
+#                                                                             #
+#*****************************************************************************#
+
+## sizein is taken into account
+DESCRIPTION="when prefix dereplicating, --uc output accounts for --sizein"
+s=$(printf ">seq1;size=3;\nACGT\n>seq2;size=1;\nACGT\n" | \
+           "${VSEARCH}" \
+               --derep_prefix - \
+               --quiet \
+               --sizein \
+               --minseqlength 1 \
+               --uc - | grep "^C" | cut -f 3)
+
+(( ${s} == 4 )) && \
+    success  "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+# clean
+unset s
+
+
+## vsearch reports H record when sequences have the same length
+DESCRIPTION="when prefix dereplicating same length sequences, --uc reports H record"
+H=$(printf ">seq1;size=3;\nACGT\n>seq2;size=1;\nACGT\n" | \
+           "${VSEARCH}" \
+               --derep_prefix - \
+               --quiet \
+               --sizein \
+               --minseqlength 1 \
+               --uc - | grep "^H")
+
+[[ -n ${H} ]] && \
+    success  "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+unset H
+
+## vsearch reports H record when sequences have different lengths
+DESCRIPTION="when prefix dereplicating a shorter sequence, --uc reports H record"
+H=$(printf ">seq1;size=3;\nACGTA\n>seq2;size=1;\nACGT\n" | \
+           "${VSEARCH}" \
+               --derep_prefix - \
+               --quiet \
+               --sizein \
+               --minseqlength 1 \
+               --uc - | grep "^H")
+
+[[ -n ${H} ]] && \
+    success  "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+unset H
+
 
 exit 0
